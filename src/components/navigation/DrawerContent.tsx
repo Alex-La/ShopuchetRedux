ae126763355f5302517d0cc67d8df3d22afd1e71
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-10-24 19:41:26 +0300

    Update DrawerContent.tsx

--- src/components/navigation/DrawerContent.tsx
+++ src/components/navigation/DrawerContent.tsx
@@ -1,14 +1,47 @@
 import {DrawerContentComponentProps} from '@react-navigation/drawer';
-import {Drawer, DrawerItem} from '@ui-kitten/components';
+import {Drawer, DrawerItem, Icon, useTheme} from '@ui-kitten/components';
 import React from 'react';
+import {ImageProps, View} from 'react-native';
+
+const ForwardIcon = (props?: Partial<ImageProps>) => (
+  <Icon {...props} name="arrow-ios-forward" />
+);
+
+const PersonIcon = (props?: Partial<ImageProps>) => (
+  <Icon {...props} name="person-outline" />
+);
+
+const LogoutIcon = (props?: Partial<ImageProps>) => (
+  <Icon {...props} name="log-out-outline" />
+);
 
 interface Props extends DrawerContentComponentProps {}
 
 const DrawerContent: React.FC<Props> = ({navigation, state}) => {
+  const theme = useTheme();
+
   return (
-    <Drawer>
-      <DrawerItem title="Выбрать торговую точку" />
+    <Drawer
+      footer={() => (
+        <>
+          <DrawerItem
+            title="Профиль"
+            accessoryLeft={PersonIcon}
+            style={{
+              borderBottomWidth: 1,
+              borderBottomColor: theme['color-basic-300'],
+            }}
+          />
+          <DrawerItem title="Выход" accessoryLeft={LogoutIcon} />
+        </>
+      )}>
+      <DrawerItem title="Выбрать торговую точку" accessoryRight={ForwardIcon} />
       <DrawerItem title="Главная" />
+      <DrawerItem title="Сводный отчет" />
+      <DrawerItem title="Торговля" />
+      <DrawerItem title="Отчеты" />
+      <DrawerItem title="Товарные остатки" />
+      <DrawerItem title="Список друзей" />
     </Drawer>
   );
 };
