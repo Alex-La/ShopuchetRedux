0280b229e7cb52c5b7abaef9492b52d2597d3da1
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-10-30 21:38:29 +0300

    Update fetchActions.ts

--- src/redux/actions/fetchActions.ts
+++ src/redux/actions/fetchActions.ts
@@ -1,12 +1,10 @@
 import {show} from '../../utils/snackbar';
 import {AppDispatch} from '../store';
-import {SetIsAuthAction} from '../types/auth.types';
 import {
   FETCH_ACTION_TYPES,
   SetAppLoadingAction,
   SetLoadingAction,
 } from '../types/fetch.types';
-import {setIsAuth} from './authActions';
 
 export const setAppLoading = (loading: boolean): SetAppLoadingAction => ({
   type: FETCH_ACTION_TYPES.SET_APP_LOADING,
@@ -22,6 +20,5 @@ export const onError =
   (status: number, text?: string) => (dispatch: AppDispatch) => {
     console.log(status);
     dispatch<SetAppLoadingAction>(setAppLoading(false));
-    if (status === 401) dispatch<SetIsAuthAction>(setIsAuth(false));
-    else if (text) show({text, type: 'error'});
+    if (status !== 401 && text) show({text, type: 'error'});
   };
