0ca6f1a7bceb9820a4e68e1132255a5f6351914c
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-11-04 01:02:09 +0300

    Update StackTopNavigation.tsx

--- src/components/navigation/StackTopNavigation.tsx
+++ src/components/navigation/StackTopNavigation.tsx
@@ -1,30 +1,47 @@
 import React from 'react';
-import {
-  Divider,
-  Icon,
-  TopNavigation,
-  TopNavigationAction,
-} from '@ui-kitten/components';
-import {ImageProps} from 'react-native';
+import {Divider, Icon, Layout, Text, useTheme} from '@ui-kitten/components';
+import {ImageProps, StyleSheet, TouchableOpacity, View} from 'react-native';
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
-
-const BackIcon = (props?: Partial<ImageProps>) => (
-  <Icon {...props} name="arrow-ios-back-outline" />
-);
 
 interface Props extends NativeStackHeaderProps {}
 
 const StackTopNavigation: React.FC<Props> = ({options, navigation}) => {
-  const BackAction = () => (
-    <TopNavigationAction icon={BackIcon} onPress={navigation.goBack} />
+  const theme = useTheme();
+
+  const BackIcon = (props?: Partial<ImageProps>) => (
+    <Icon
+      {...props}
+      name="arrow-ios-back-outline"
+      fill={theme['text-basic-color']}
+    />
   );
 
   return (
     <>
-      <TopNavigation accessoryLeft={BackAction} title={options.title} />
+      <Layout style={styles.wrap}>
+        <TouchableOpacity onPress={navigation.goBack}>
+          <BackIcon width={25} height={25} />
+        </TouchableOpacity>
+        <View style={{marginLeft: 16}}>
+          <Text category="s1">{options.title}</Text>
+          {/* {name !== 'Profile' && name !== 'Friends' && subtitle && (
+            <Text category="label" appearance="hint">
+              {subtitle}
+            </Text>
+          )} */}
+        </View>
+      </Layout>
       <Divider />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  wrap: {
+    flexDirection: 'row',
+    padding: 16,
+    alignItems: 'center',
+  },
+});
 
 export default StackTopNavigation;
